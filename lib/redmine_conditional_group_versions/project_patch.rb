78608bdd6e53690509c67fe7520c7abc94713176
Farkwun <bryan.chu@ryerson.ca>
2016-06-29 14:14:04 -0400

    Refactor for Rails 4.1 change
    In Rails 4.1, ActiveRecord::Relation no longer supports Array-like
    methods
    
    Refactoring to support Redmine 2.x and 3.x

--- lib/redmine_conditional_group_versions/project_patch.rb
+++ lib/redmine_conditional_group_versions/project_patch.rb
@@ -8,27 +8,30 @@ module RedmineConditionalGroupVersions
       end
 
       module InstanceMethods
+        # enforces conditional group visibilities on shared_versions
         def shared_versions_with_conditional_group_versions
           the_versions = shared_versions_without_conditional_group_versions
           unless group_conditional_custom_field.nil?
-            the_versions.reject! {|v| !user_group_can_see?(v)} 
+            # because ActiveRecord::Relation no longer supports 
+            # Array-like methods, must convert to Array then back
+            the_versions.to_a.reject! {|v| !user_group_can_see?(v)} 
+            the_versions = Version.where(id: the_versions.map(&:id))
           end
           the_versions
         end
 
-
+        # checks if the user's groups can see the input version
         def user_group_can_see?(version)
           @visibilities ||= user_group_project_visibilities
           version_project = Project.find_by_id(version.project_id).name
           if is_a_conditional_version_project?(version_project)
-            logger.warn(version_project)
-            logger.warn @visibilities.include?(version_project)
             @visibilities.include?(version_project)
           else
             true
           end
         end
 
+        # returns a set of all the version projects the user can see
         def user_group_project_visibilities
           visibilities = Set.new
           User.current.groups.each do |g|
@@ -39,11 +42,13 @@ module RedmineConditionalGroupVersions
           visibilities
         end
 
+        # checks if the input project is a conditional version projects
         def is_a_conditional_version_project?(project_name)
           @version_projects ||= group_conditional_custom_field.possible_values
           @version_projects.include?(project_name)
         end
 
+        # returns group custom field specified in settings
         def group_conditional_custom_field
           list_option = Setting.plugin_redmine_conditional_group_versions[:group_custom_list_field]
           return if list_option.eql?('none')
