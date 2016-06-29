175a6f7e7f0a6282eb52a5bce43a49ec8cb0e353
Farkwun <bryan.chu@ryerson.ca>
2016-06-29 14:34:23 -0400

    Fixing whitespace errors

--- lib/redmine_conditional_group_versions/projects_helper_patch.rb
+++ lib/redmine_conditional_group_versions/projects_helper_patch.rb
@@ -1,21 +1,21 @@
 module RedmineConditionalGroupVersions
-	module Patches
-		module ProjectsHelperPatch
-			def self.included(base)
-				base.send(:include, InstanceMethods)
+  module Patches
+    module ProjectsHelperPatch
+      def self.included(base)
+        base.send(:include, InstanceMethods)
 
-				base.alias_method_chain :version_options_for_select, :group_conditional_versions
-			end
+        base.alias_method_chain :version_options_for_select, :group_conditional_versions
+      end
 
-			module InstanceMethods
+      module InstanceMethods
         # enforces conditional group visibilities in an issue's edit menu
-				def version_options_for_select_with_group_conditional_versions(versions, selected=nil)
+        def version_options_for_select_with_group_conditional_versions(versions, selected=nil)
           whitelist_versions = @project.shared_versions.to_a & versions
-					version_options_for_select_without_group_conditional_versions(whitelist_versions, selected)
-				end
-			end
-		end
-	end
+          version_options_for_select_without_group_conditional_versions(whitelist_versions, selected)
+        end
+      end
+    end
+  end
 end
 
 
